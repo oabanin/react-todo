2517de3c69226a7e869c9d4e1adc8f5944821411
oabanin-HOME <juristaba@gmail.com>
2020-02-08 16:52:56 +0300

    send to react components different types of data via props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  </div>
-);
 
-ReactDOM.render(el,
-  document.getElementById('root'));
+const TodoList = (props) => {
+    const items = props.items;
+    const itemList = items.map((item, index) => <li key={index}>{item}</li>);
+    return (
+        <ul>
+            {itemList}
+        </ul>
+    )
+};
+
+const AppHeader = () => (<h1>My Todo List</h1>);
+
+const SeachPanel = (props) => {
+    console.log(props);
+    const searchText = 'Type Here to seracj';
+    return (<input placeholder={props.date } />)};
+
+const isLoggedIn = true;
+const loginBox = <span>Login Box</span>;
+const App = () => (
+    <div>
+        <AppHeader  />
+        {true}
+        {false}
+        {undefined}
+        {}
+        {loginBox}
+        <SeachPanel date={new Date()}/>
+        <TodoList items={['Learn React', 'Build Awesome App']} />
+    </div>);
+
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
