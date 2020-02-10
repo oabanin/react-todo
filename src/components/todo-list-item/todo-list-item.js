8a8cd4c7974b0e5f22f01da7aa7362b2cdb28ad0
oabanin-WORK <juristaba@gmail.com>
2020-02-10 17:34:49 +0400

    add event handler to click on item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,31 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
 
-export default class TodoListItem extends Component{
-  render(){
+export default class TodoListItem extends Component {
+
+  onLabelClick = () => {
+    console.log(this.props.label);
+  }
+
+  render() {
     const { label, important = false } = this.props;
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
-  
+
     return (
       <span className="todo-list-item">
         <span
+          onClick={this.onLabelClick}
           className="todo-list-item-label"
           style={style}>
           {label}
         </span>
-  
+
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+          className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-exclamation" />
         </button>
-  
+
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+          className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
         </button>
       </span>
